9b69214f7442973ade799ad20a72730409108088
MehItsMads <161098561+MehItsMads@users.noreply.github.com>
2026-03-04 17:11:11 -0600

    Update main.js
    added rainbow curser

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
 // main.js
 
 console.log("Hello, World!");
+
+const cursor = document.createElement("div");
+cursor.classList.add("custom-cursor");
+document.body.appendChild(cursor);
+
+let hue = 0;
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+  
+  hue += 2;
+  cursor.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
+});
